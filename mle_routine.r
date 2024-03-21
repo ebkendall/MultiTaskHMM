da70a596cc21147076ab11d98cb700f37121156a
ebkendall <ebkendal@ncsu.edu>
2024-03-21 16:10:34 -0400

    started writing the components of the Baum Welch

---
+++ mle_routine.r
@@ -0,0 +1,71 @@
+library(mvtnorm, quietly=T) 
+library(foreach, quietly=T) 
+library(doParallel, quietly=T)
+
+log_likelihood_fnc <- function(par, par_index, y, id) {
+
+    # Initial state probabilities
+    init_logit = c(1, exp(pars[par_index$logit_pi][1]), 
+                      exp(pars[par_index$logit_pi][2]))
+    init = init_logit / sum(init_logit)
+
+    m_1 = par[par_index$mu_1] # mean for state 1
+    m_2 = par[par_index$mu_2] # mean for state 2
+    m_3 = par[par_index$mu_3] # mean for state 3
+
+    cov_1 = diag(exp(par[par_index$log_sig_1])) # covariance for state 1
+    cov_2 = diag(exp(par[par_index$log_sig_2])) # covariance for state 2
+    cov_3 = diag(exp(par[par_index$log_sig_3])) # covariance for state 3
+
+    # Transition probability matrix
+    q1   = exp(par[par_index$logit_t_p][1])
+    q2   = exp(par[par_index$logit_t_p][2])
+    q3   = exp(par[par_index$logit_t_p][3])
+    q4   = exp(par[par_index$logit_t_p][4])
+    q5   = exp(par[par_index$logit_t_p][5])
+    q6   = exp(par[par_index$logit_t_p][6])
+
+    Q = matrix(c(1,  q1, q2,
+                q3,   1, q4,
+                q5,  q6,  1), ncol=3, byrow=T)
+    P = Q / rowSums(Q)
+
+    # Parallelized computation of the log-likelihood
+    log_total_val = foreach(i=unique(id), .combine='+') %dopar% {
+
+        y_i = y[id == i, , drop = F]  # the matrix of responses for subject i
+
+        d_1 = dmvnorm(y_i[1, ], mean = m_1, sigma = cov_1)
+        d_2 = dmvnorm(y_i[1, ], mean = m_2, sigma = cov_2)
+        d_3 = dmvnorm(y_i[1, ], mean = m_3, sigma = cov_3)
+
+        f_i = init %*% diag(c(d_1, d_2, d_3))
+        log_norm = 0
+        val = 1
+
+        for(k in 2:nrow(y_i)) {
+
+            d_1 = dmvnorm(y_i[k, ], mean = m_1, sigma = cov_1)
+            d_2 = dmvnorm(y_i[k, ], mean = m_2, sigma = cov_2)
+            d_3 = dmvnorm(y_i[k, ], mean = m_3, sigma = cov_3)
+
+            val = f_i %*% P %*% diag(c(d_1, d_2, d_3))
+
+            norm_val = sqrt(sum(val^2))
+            f_i = val / norm_val
+            log_norm = log_norm + log(norm_val)
+        }
+
+        return(log(sum(f_i)) + log_norm)
+    }
+
+    return(log_total_val)
+}
+
+forward_proc <- function(par, par_index, y, id) {
+    
+}
+
+backward_proc <- function(par, par_index, y, id) {
+
+}
---
+++ mle_runfile.r
@@ -0,0 +1,37 @@
+source("mle_routine.r")
+
+args = commandArgs(TRUE)
+ind = as.numeric(args[1])
+
+set.seed(ind)
+print(ind)
+
+simulation = T
+
+# Initial values
+init_val = c(-2, -2, -2, -2, -2, -2, # logit transition probs
+             -2, -2,                 # logit initial probabilities
+              0, 0, 0, 0, 0,         # mu_1
+              2, 2, 2, 2, 2,         # mu_2
+              4, 4, 4, 4, 4,         # mu_3
+              0, 0, 0, 0, 0,         # log_sigma_1
+              0, 0, 0, 0, 0,         # log_sigma_2
+              0, 0, 0, 0, 0)         # log_sigma_3
+
+# Indexing of the parameter vector
+par_index = list(logit_t_p = 1:6, logit_pi = 7:8, 
+                 mu_1 = 9:13, mu_2 = 14:18, mu_3 = 19:23, 
+                 log_sig_1 = 24:28, log_sig_2 = 29:33, log_sig_3 = 34:38)
+
+id = y = NULL
+
+if(simulation) {
+    # Load simulated data
+    load(paste0('Data/sim_data_', ind, '.rda'))
+
+    id = sim_data[,'id']
+    y  = sim_data[,c('y1', 'y2', 'y3', 'y4', 'y5')]
+} else {
+    # Load whatever real data we get
+}
+
